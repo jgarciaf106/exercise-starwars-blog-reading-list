b1712389e43cc3e120782cb188b32956a6b8a1a0
jgarciaf106 <jgarciaf106@gmail.com>
2021-03-21 00:52:08 +0000

    Card Props adjusted

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -9,33 +9,31 @@ export const Card = props => {
 	const { actions, store } = useContext(Context);
 
 	return (
-		<div className="scrolling-wrapper row flex-row flex-nowrap mx-3">
-			<div className="card col-4 my-4 mx-3" style={{ width: "18rem" }}>
-				<img src={props.obj.imageUrl} className="card-img-top mt-3" alt="..." />
-				<div className="card-body">
-					<h5 className="card-title">{props.obj.name}</h5>
-					<p className="card-text">Gender: {props.obj.gender}</p>
-					<p className="card-text">Hair Color: {props.obj.hairColor}</p>
-					<p className="card-text">Eye-Color: {props.obj.eyeColor}</p>
-					<div className="row">
-						<div className="col">
-							<Link exact to="/detail">
-								<a
-									href="#"
-									className="btn btn-warning float-left"
-									onClick={() => actions.setIdSelected(props.obj.id)}>
-									Learn more!
-								</a>
-							</Link>
+		<div className="card col-4 my-4 mx-3" style={{ width: "18rem" }}>
+			<img src={props.obj.imageUrl} className="card-img-top mt-3" alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">{props.obj.name}</h5>
+				<p className="card-text">Gender: {props.obj.gender}</p>
+				<p className="card-text">Hair Color: {props.obj.hairColor}</p>
+				<p className="card-text">Eye-Color: {props.obj.eyeColor}</p>
+				<div className="row">
+					<div className="col">
+						<Link exact to="/detail">
+							<a
+								href="#"
+								className="btn btn-warning float-left"
+								onClick={() => actions.setIdSelected(props.obj.id)}>
+								Learn more!
+							</a>
+						</Link>
 
-							<button
-								type="button"
-								className="btn btn-warning float-right"
-								onClick={() => actions.setFavorites(props.obj.id)}
-								disabled={store.favorites.includes(props.obj.id) ? true : false}>
-								<i className={store.favorites.includes(props.obj.id) ? "fas fa-heart" : "far fa-heart"} />
-							</button>
-						</div>
+						<button
+							type="button"
+							className="btn btn-warning float-right"
+							onClick={() => actions.setFavorites(props.obj.id)}
+							disabled={store.favorites.includes(props.obj.id) ? true : false}>
+							<i className={store.favorites.includes(props.obj.id) ? "fas fa-heart" : "far fa-heart"} />
+						</button>
 					</div>
 				</div>
 			</div>
--- src/js/views/main.js
+++ src/js/views/main.js
@@ -11,16 +11,19 @@ export const Main = () => {
 	return (
 		<div className="text-center bg-dark container">
 			<Category category="Characters" />
-			{store.people.map((person, index) => {
-				const personObj = {
-					name: person.id: person.id,
-					gender: person.gender,
-					hairColor: person.hairColor,
-					eyeColor: person.eyeColor,
-					imageUrl: person.image
-				};
-				return <Card obj={personObj} key={index} />;
-			})}
+			<div className="scrolling-wrapper row flex-row flex-nowrap mx-3">
+				{store.people.map((person, index) => {
+					const personObj = {
+						id: person.id,
+						name: person.name,
+						gender: person.gender,
+						hairColor: person.hairColor,
+						eyeColor: person.eyeColor,
+						imageUrl: person.image
+					};
+					return <Card obj={personObj} key={index} />;
+				})}
+			</div>
 			<Category category="Vehicles" />
 			<Category category="Planets" />
 		</div>
